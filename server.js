cf728969d00a38cad50fff18a99389e93ba1e377
Yuvi110 <109853694+Yuvi110@users.noreply.github.com>
2023-07-13 14:23:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js";
 import cors from "cors";
 import path from "path";
-import {fileURLToPath} from 'url
+import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
