181e0985791a32a12ec69688a5fea0ad0eba216c
Yuvi110 <109853694+Yuvi110@users.noreply.github.com>
2023-07-13 14:21:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js";
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from 'url
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
